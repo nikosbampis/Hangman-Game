22a7412e199d4b0853c88b38486f51278789291a
Nikos <babis.nick@hotmail.gr>
2020-09-03 15:53:42 +0300

    End of day

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,27 +1,64 @@
 import string
 
+#Type word
 #wordHangman = input(str("Give word for Hangman:"))
-wordHangman = "test"
-print(wordHangman)
+wordHangman = "aesttttt"
+#print(wordHangman)
+wordHangmanList = list(wordHangman)
+secretWord = list(wordHangman)
 
-wordHangman = list(wordHangman)
-
-
+#Count letters of secret word
 result = 0
 for char in wordHangman:
     result += 1
 
 #print(result)...
 
+#Alphabetic letters
 letters = string.ascii_lowercase
 letters = list(letters)
-print(letters)
-
-count_letters = list(range(1, len(wordHangman)))
-print(count_letters)
+#print(letters)
 
 
+count_letters = list(range(1, len(secretWord)))
+#print(count_letters)
+
+#Replace all letters except the first one with _
 for char in count_letters:
-    wordHangman[char] = "_"
+    secretWord[char] = "_"
 
-print(wordHangman)
+#print(secretWord)
+
+
+for i in range(8):
+    inputLetter = input(str("Give Letter: "))
+
+    # Ask for only ONE letter (need correction)???
+    if len(inputLetter) >= 2:
+        print("Type only ONE letter")
+        inputLetter = input(str("Give Letter: "))
+    else:
+        print("The letter you type is", inputLetter)
+
+    #Remove typing letter from alphabetic list
+    for lowerc in letters:
+        if lowerc == inputLetter:
+            letters.remove(lowerc)
+    #print(list((range(0, len(wordHangmanList)))))
+    #exit()
+    #print(secretWord)
+
+
+    #Add letter to secret word if it correct
+    for letter in wordHangmanList:
+        if letter == inputLetter:
+            wordIdx = wordHangmanList.index(letter)
+            secretWord[wordIdx] = inputLetter
+    print(secretWord)
+
+
+    #str = ""
+    #print(str.join(secretWord))
+
+
+#print(letters)
