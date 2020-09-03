5c9f3f603c927d7e591df2c016897b0b2258bc45
Nikos <babis.nick@hotmail.gr>
2020-09-03 13:44:17 +0300

    Hide all letters except the first one

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,14 +1,27 @@
+import string
 
-
-wordHangman = input(str("Give word for Hangman:"))
+#wordHangman = input(str("Give word for Hangman:"))
+wordHangman = "test"
 print(wordHangman)
+
+wordHangman = list(wordHangman)
+
 
 result = 0
 for char in wordHangman:
-    result +=1
+    result += 1
 
-#print(result)..
+#print(result)...
+
+letters = string.ascii_lowercase
+letters = list(letters)
+print(letters)
+
+count_letters = list(range(1, len(wordHangman)))
+print(count_letters)
 
 
+for char in count_letters:
+    wordHangman[char] = "_"
 
-
+print(wordHangman)
