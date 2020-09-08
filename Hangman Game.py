59fb9aee25c6f90756c3cf7da748f9395377398c
Nikos <babis.nick@hotmail.gr>
2020-09-08 10:39:58 +0300

    Find index of all input chars

--- Hangman Game.py
+++ Hangman Game.py
@@ -1,29 +1,46 @@
 import string
 
-#Type word
-#wordHangman = input(str("Give word for Hangman:"))
-wordHangman = "aesttttt"
+def list_duplicates_of(seq,item):
+    start_at = -1
+    locs = []
+    while True:
+        try:
+            loc = seq.index(item, start_at+1)
+        except ValueError:
+            break
+        else:
+            locs.append(loc)
+            start_at = loc
+    return locs
+
+
+# print(list_duplicates_of(source, 'B'))
+# exit()
+# Type word
+# wordHangman = input(str("Give word for Hangman:"))
+wordHangman = "abbbsfsagcc"
+print(wordHangman)
 
 wordHangmanList = list(wordHangman)
 secretWord = list(wordHangman)
 
-#Count letters of secret word
+# Count letters of secret word
 result = 0
 for char in wordHangman:
     result += 1
 
-#print(result)...
+# print(result)...
 
-#Alphabetic letters
+# Alphabetic letters
 letters = string.ascii_lowercase
 letters = list(letters)
-#print(letters)
+# print(letters)
 
 
 count_letters = list(range(1, len(secretWord)))
-#print(count_letters)
 
-#Replace all letters except the first one with _
+
+# Replace all letters except the first one with _
 for char in count_letters:
     secretWord[char] = "_"
 
@@ -39,24 +56,41 @@ for i in range(8):
     else:
         print("The letter you type is", inputLetter)
 
-    #Remove typing letter from alphabetic list
+    # Remove typing letter from alphabetic list
     for lowerc in letters:
         if lowerc == inputLetter:
             letters.remove(lowerc)
-    #print(list((range(0, len(wordHangmanList)))))
-    #print(secretWord)
+    # print(list((range(0, len(wordHangmanList)))))
+    # print(secretWord)
+    str_join = ","
+    print(str_join.join(letters))
+    letters = list(letters)
 
 
-    #Add letter(s) to secret word if it correct
-    for letter in wordHangmanList:
-        if letter == inputLetter:
-            wordIdx = wordHangmanList.index(letter)
-            secretWord[wordIdx] = inputLetter
-    print(secretWord)
+    # Add letter(s) to secret word if it correct
+    # for letter in wordHangmanList:
+    #     if letter == inputLetter:
+    #         wordIdx = wordHangmanList.index(letter)
+    #         secretWord[wordIdx] = inputLetter
+
+    if inputLetter in wordHangman:
+        wordIdx = list_duplicates_of(wordHangman, inputLetter)
+        for i in wordIdx:
+            secretWord[i] = inputLetter
 
 
-    #str = ""
-    #print(str.join(secretWord))
+    word_join = ""
+    print(word_join.join(secretWord))
+    #print(wordHangman)
 
 
-#print(letters)
+    if secretWord == wordHangmanList:
+        print("Success")
+        break
+
+
+    # str = ""
+    # print(str.join(secretWord))
+
+
+# print(letters)
